6fba71fda12b7690ff61b48f6c0513a39479dd24
FireCubeStudios <40955345+FireCubeStudios@users.noreply.github.com>
2025-02-21 02:27:19 +0100

    Use integer division

--- Medium/ReverseInteger.py
+++ Medium/ReverseInteger.py
@@ -2,19 +2,17 @@
 
 class Solution:
     def reverse(self, x: int) -> int:
-        num = abs(x) # Convert to unsigned integer
+        num: int = abs(x) # Convert to unsigned integer
 
         # Use modulo to reverse integer by extracting digits
         # Then add those digits to new number while ensuring correct placeholder
-        i = 10
-        prev = 0
         y = 0
         while True:
-            if num / (i / 10) < 1: break # Exit loop when all digits reversed
-            digit = (int)(((num % i) - prev) / (i / 10))
+            if num < 1: break # Exit loop when all digits reversed
+            digit = num % 10
+            num //= 10 # The //= operator performs integer division which rounds to 0 aka 57 / 10 = 5
             y *= 10
             y += digit
-            i *= 10
 
         y *= 1 if x > 0 else -1 # Restore original signage of integer
         if y > -2**31 and y < 2**31 - 1: # Check if result is within 32-bit integer range
@@ -22,4 +20,4 @@ class Solution:
         else: 
             return 0 # If answer goes outside signed 32-bit integer range then return 0
         
-print(Solution().reverse(1234))
+print(Solution().reverse(1234)) # ans = 4321
